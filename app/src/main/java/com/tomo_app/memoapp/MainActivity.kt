1c314d23b42b64e65eaa76ea5c352030438022d0
Potaton1155 <89030575+Potaton1155@users.noreply.github.com>
2022-07-05 22:55:48 +0900

    ad 表示回数設定

--- app/src/main/java/com/tomo_app/memoapp/MainActivity.kt
+++ app/src/main/java/com/tomo_app/memoapp/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.tomo_app.memoapp
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.ads.AdListener
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.MobileAds
 import com.tomo_app.memoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
+    private var adTappedCount: Int = 0
 
     override fun onCreate(savedinstancestate: Bundle?) {
         super.onCreate(savedinstancestate)
@@ -21,5 +25,20 @@ class MainActivity : AppCompatActivity() {
         val adRequest = AdRequest.Builder().build()
         binding.adView.loadAd(adRequest)
 
+        binding.adView.adListener = object : AdListener() {
+            override fun onAdClicked() {
+                super.onAdClicked()
+                adTappedCount += 1
+                when {
+                    adTappedCount > 2 -> binding.adView.visibility = View.GONE
+                }
+            }
+        }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            binding.adView.visibility = View.VISIBLE
+            adTappedCount = 0
+        }, 500L)
     }
+
 }
