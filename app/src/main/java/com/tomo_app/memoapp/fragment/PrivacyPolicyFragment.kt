88e847b7f480217c4b82d41f57c441afb4192d6e
Potaton1155 <89030575+Potaton1155@users.noreply.github.com>
2023-06-24 19:22:52 +0900

    プライバシーポリシー追加

---
+++ app/src/main/java/com/tomo_app/memoapp/fragment/PrivacyPolicyFragment.kt
@@ -0,0 +1,40 @@
+package com.tomo_app.memoapp.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.tomo_app.memoapp.databinding.FragmentPrivacyPolicyBinding
+
+class PrivacyPolicyFragment : Fragment() {
+    private var _binding: FragmentPrivacyPolicyBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        _binding = FragmentPrivacyPolicyBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            backButton.setOnClickListener {
+                findNavController().popBackStack()
+            }
+            backButton2.setOnClickListener {
+                findNavController().popBackStack()
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
